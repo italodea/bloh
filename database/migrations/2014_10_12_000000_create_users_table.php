0d8c9e0b006a3a707e3b936b433adb72bb8dbae9
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-10-23 00:23:33 +0700

    Modificação na tabela users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('lastName');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('git');
+            $table->string('website');
+            $table->string('facebook');
             $table->rememberToken();
             $table->timestamps();
         });
