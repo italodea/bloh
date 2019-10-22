45385e52cc5f1fb1ef598e1774fac316f9ba8635
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-10-22 23:16:48 +0700

    estado inicial com apenas uma view

---
+++ resources/views/index.blade.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<title>TEste</title>
+	<!-- Fonts -->
+	<!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    
+
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      
+   	<script src="https://kit.fontawesome.com/2ca0972b2c.js" crossorigin="anonymous"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+	<!-- Styles -->
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links > a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+
+
+
+</head>
+<body>
+	<!-- Dropdown Structure -->
+<ul id="dropdown1" class="dropdown-content">
+  <li><a href="#!">one</a></li>
+  <li><a href="#!">two</a></li>
+  <li class="divider"></li>
+  <li><a href="#!">three</a></li>
+</ul>
+<div class="navbar-fixed">
+	<nav class="black">
+	  <div class="nav-wrapper">
+	    <a href="#!" class="brand-logo">Logo</a>
+	    <ul class="right hide-on-med-and-down">
+	      <li><a href="sass.html"><i class="far fa-bell"></i></a></li>
+	      <li><a href="badges.html"><i class="fas fa-search"></i></a></li>
+	      <!-- Dropdown Trigger -->
+	      <!-- <i class="fab fa-buromobelexperte"></i> -->
+	      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1"><i class="fas fa-th" ></i></a></li>
+	    </ul>
+	  </div>
+	</nav>
+</div>
+
+<br><br><br>
+
+<div class="row">
+	<div class="col offset-l1 l10 s12">
+		<div class="card z-depth-2">
+			<div class="row">
+				<div class="col l1 s2">
+					<img src="https://robohash.org/jrklje?set=set2" alt="" class="circle responsive-img">
+				</div>
+				<div class="col s10">
+					<h6>Name of the person say</h6>
+				</div>
+				<div class="col s10">
+					published at: 18:06pm
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-m1 offset-s1 offset-l1 l10">
+					<code>
+					$ pip install beautifulsoap
+					</code>	
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-s1" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-heart">12</i>
+				</div>
+				<div class="col" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-comments">5</i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row ">
+	<div class="col offset-l1 l10 s12 ">
+		<div class="card z-depth-2">
+			<div class="row">
+				<div class="col l1 s2">
+					<img src="https://robohash.org/windows9?set=set2" alt="" class="circle responsive-img">
+				</div>
+				<div class="col s10">
+					<h6>Name of the person say</h6>
+				</div>
+				<div class="col s10">
+					published at: 18:06pm
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-m1 offset-s1 offset-l1 l10">
+					<code>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nost<b>brud</b> exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					</code>
+					<br>
+					<img src="https://robohash.org/italodea?set=set2" alt="" class=" responsive-img">
+					<img src="https://robohash.org/italodea?set=set2" alt="" class=" responsive-img">
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-s1" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-heart">12</i>
+				</div>
+				<div class="col" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-comments">5</i>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<div class="col offset-l1 l10 s12 ">
+		<div class="card z-depth-2">
+			<div class="row">
+				<div class="col l1 s2">
+					<img src="https://robohash.org/nsk99?set=set2" alt="" class="circle responsive-img">
+				</div>
+				<div class="col s10">
+					<h6>Name of the person say</h6>
+				</div>
+				<div class="col s10">
+					published at: 18:06pm
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-m1 offset-s1 offset-l1 l10">
+					<code>
+					Aqui agora vai um video bem legal.<br>
+					Espero que gostem
+					</code>
+					<br>
+      				<div class="video-container row">
+        				<iframe class="col s12" src="https://www.youtube.com/embed/0rkPnaVppbQ?rel=0" frameborder="0" allowfullscreen></iframe>
+      				</div>	
+				</div>
+			</div>
+			<div class="row">
+				<div class="col offset-s1 s1" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-heart">12</i>
+				</div>
+				<div class="col s1" style="color: black;">
+  					&nbsp;&nbsp;&nbsp;<i class="fas fa-lg fa-comments">5</i>
+				</div>
+				<div class="col offset-s7 s1" >
+  					&nbsp;&nbsp;&nbsp;<a href="#" style="color: black;"><i class="fas fa-lg fa-arrow-right"></i></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<script type = "text/javascript" src = "https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> 
+<script type="text/javascript">
+	$(document).ready(function(){
+		$(".dropdown-trigger").dropdown();
+	})
+</script>
+
+</body>
+</html>
