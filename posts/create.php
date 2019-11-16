0e7b07afb39bde5a12c68aca50da6b4ca28133ae
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-16 23:58:22 +0700

    resolvido bug com textos com aspas simples

--- posts/create.php
+++ posts/create.php
@@ -5,8 +5,10 @@ session_start();
 require('../etc/connection.php');
 
 $conteudo = nl2br($_POST['contentText']);
-$query = "INSERT INTO posts(`author`,`mainContent`,`created_at`) VALUES ('".$_SESSION['id']."','".$conteudo."',NOW())";
+$query = 'INSERT INTO posts(`author`,`mainContent`,`created_at`) VALUES ('.$_SESSION['id'].',"'.$conteudo.'",NOW())';
 $run = mysqli_query($con,$query);
+echo $query;
+
 header("Location:/");
 
 ?>
