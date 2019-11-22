015c5c8c000bdf59e03b342b6b9cd291aee0d7db
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-22 22:44:39 +0700

    adição de indentação no codigo

--- posts/update.php
+++ posts/update.php
@@ -1,12 +1,12 @@
 <?php 
 	
 
-session_start();
-require('../etc/connection.php');
+	session_start();
+	require('../etc/connection.php');
 
-$sql = 'UPDATE posts SET `mainContent` = "'.$_POST['contentText'].'"WHERE id ='.$_GET['id'].';';
-$run = mysqli_query($con,$sql);
-echo "$sql";
-header("Location:/");
+	$sql = 'UPDATE posts SET `mainContent` = "'.$_POST['contentText'].'"WHERE id ='.$_GET['id'].';';
+	$run = mysqli_query($con,$sql);
+	echo "$sql";
+	header("Location:/");
 
 ?>
