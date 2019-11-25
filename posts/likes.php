a57267174a80214013a1f5c58ed17c800b5ce8f0
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-26 00:04:02 +0700

    Agora os likes sobem e descem de automaticamente

--- posts/likes.php
+++ posts/likes.php
@@ -1,10 +1,43 @@
 <?php
-return "oi";
+
+
 session_start();
 
 require ('../etc/connection.php');
 
-$query = 'INSERT INTO posts(`author`,`mainContent`,`created_at`) VALUES ('.$_SESSION['id'].',"'.$_POST['id'].'",NOW())';
+$query = 'SELECT * FROM likes WHERE user ='. $_SESSION['id'] .' AND post = '. $_POST['id'] .';';
 $run = mysqli_query($con,$query);
+if (mysqli_num_rows($run) == 1){
+	$query = 'DELETE FROM likes WHERE user ='. $_SESSION['id'] .' AND post = '. $_POST['id'] .';';
+	$run = mysqli_query($con,$query);
+	
+	$query = 'SELECT count(*) from likes WHERE post = '. $_POST['id'] .'; ';
+	$run = mysqli_query($con,$query);
 
+	$count = mysqli_fetch_array($run);
+
+	$query = 'UPDATE posts SET likes = '.$count['count(*)'].' WHERE id = '.$_POST['id'].';';
+	$run = mysqli_query($con,$query);
+
+	echo $count['count(*)'];
+	return false;
+	
+}else{
+	$query = 'INSERT INTO likes(`user`,`post`,`dateTime`) VALUES('. $_SESSION['id'] .', '. $_POST['id'] .', NOW());';
+	$run = mysqli_query($con,$query);
+	
+	$query = 'SELECT count(*) from likes WHERE post = '. $_POST['id'] .'; ';
+	$run = mysqli_query($con,$query);
+
+	$count = mysqli_fetch_array($run);
+
+	$query = 'UPDATE posts SET likes = '.$count['count(*)'].' WHERE id = '.$_POST['id'].';';
+	$run = mysqli_query($con,$query);
+
+	echo $count['count(*)'];
+	return false;
+}
+
+//$query = 'INSERT INTO posts(`author`,`mainContent`,`created_at`) VALUES ('.$_SESSION['id'].',"'.$_POST['id'].'",NOW())';
+//$run = mysqli_query($con,$query);
 ?>
