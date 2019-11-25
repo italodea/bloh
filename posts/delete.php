33c4d4447c77bbe3c3401a11f6e95dc22295c832
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-26 00:04:27 +0700

    Correção de bug que impedia a deletação de um post

--- posts/delete.php
+++ posts/delete.php
@@ -6,7 +6,11 @@
 	require('../etc/connection.php');
 	$sql = "SELECT author FROM  posts where id = ".$idPost." AND author =".$_SESSION['id'].";";
 	$run = mysqli_query($con,$sql);
+	echo $sql;
 	if ( mysqli_num_rows($run) == 1){
+		$sql = 'DELETE FROM likes where post = .$idPost.;'
+		$run = mysqli_query($con,$sql);
+		
 		$sql = "DELETE FROM posts where id = ".$idPost.";";
 		$run = mysqli_query($con,$sql);
 		header("Location:/");
