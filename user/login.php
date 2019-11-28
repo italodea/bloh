70b3ca0963c961e25c421ec3b15f50dd4de97ec9
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-28 09:23:00 +0700

    Agora ao fazer login será registrado informações para fins de segurança. Será salvo: * IP * ID do usuário * Data e hora do login

--- user/login.php
+++ user/login.php
@@ -13,6 +13,28 @@ function verifyData(){
 		return False;
 	}
 }
+
+function get_client_ip() {
+    $ipaddress = '';
+    if (isset($_SERVER['HTTP_CLIENT_IP']))
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_X_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if(isset($_SERVER['HTTP_FORWARDED']))
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if(isset($_SERVER['REMOTE_ADDR']))
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+
+
+
 if (($varri = verifyData()) == True){
 	require('../etc/connection.php');
 	$string = "SELECT * FROM users WHERE `email` = '".$_POST['email']."' AND `password` ='".md5($_POST['password'])."'";
@@ -20,11 +42,16 @@ if (($varri = verifyData()) == True){
 	$data = mysqli_fetch_array($query);
 	
 	if (mysqli_num_rows($query) == 1) {
+		$query = 'INSERT INTO loginhistory(`ip`,`user`,`dateTime`) VALUES (INET_ATON("'. get_client_ip().'"),'.$data['id'].',NOW())';
+		$run = mysqli_query($con,$query);
+
 		$_SESSION['id'] = $data['id'];
 		$_SESSION['name'] = $data['name'];
 		$_SESSION['lastName'] = $data['lastName'];
 		$_SESSION['email'] = $data['id'];
-		echo "1";
+
+
+		echo "1";//sucess
 	}else{
 		unset ($_SESSION['id']);
   		unset ($_SESSION['name']);
