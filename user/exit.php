6e0f94077e839e3b7f8d7680993a53759dab63ff
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-28 13:26:30 -0300

    Agora o status da sessão é alterada no banco de dados ao finalizar

--- user/exit.php
+++ user/exit.php
@@ -1,6 +1,11 @@
 <?php 
 session_start();
+if ($_SESSION['token']){
+	require('../etc/connection.php');
+	$query = 'UPDATE loginhistory SET `status` = "ended" WHERE `sessionName` ="'.$_SESSION['token'].'";';
+	$run = mysqli_query($con,$query);
+}
 session_unset();
 session_destroy();
-header("Location:/welcome.php")
+header("Location:/welcome.php");
 ?>
