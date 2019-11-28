bf84eb90cbcb7887a7ce8945dee375ee864ec14a
italo de azevedo albuquerque <italodeazevedo99@gmail.com>
2019-11-28 13:27:51 -0300

    Adição de token e status na tabela de logins

--- backupSQL/blogdb_loginhistory.sql
+++ backupSQL/blogdb_loginhistory.sql
@@ -27,9 +27,10 @@ CREATE TABLE `loginhistory` (
   `ip` int(11) unsigned NOT NULL,
   `user` bigint(20) unsigned NOT NULL,
   `status` varchar(20) NOT NULL,
+  `sessionName` char(32) NOT NULL,
   `dateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -41,4 +42,4 @@ CREATE TABLE `loginhistory` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-28  9:23:19
+-- Dump completed on 2019-11-28 13:26:44
